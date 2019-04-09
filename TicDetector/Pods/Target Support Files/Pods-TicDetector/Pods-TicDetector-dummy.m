d41b87d3725f0a0f667127560a3bada3600fc8f6
Deniz Mersinlioğlu <mersinliogludeniz@gmail.com>
2019-04-10 00:57:09 +0300

    iOS add appended

---
+++ TicDetector/Pods/Target Support Files/Pods-TicDetector/Pods-TicDetector-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TicDetector : NSObject
+@end
+@implementation PodsDummy_Pods_TicDetector
+@end
---
+++ TicDetector/TicDetector/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  TicDetector
+//
+//  Created by Deniz Mersinlioğlu on 4/8/19.
+//  Copyright © 2019 DevelopmentHouse. All rights reserved.
+//
+
+import UIKit
+import GRTiOS
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        let asd = GestureRecognitionPipeline()
+    }
+
+
+}
+
